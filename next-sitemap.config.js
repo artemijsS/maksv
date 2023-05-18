bfe213050c72f2900e91c4d184b50acc6ca480f8
artemijsS <artjom1408@gmail.com>
2023-05-18 17:15:24 +0300

    sitemap admin excleude

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://www.maksv.lv',
     generateRobotsTxt: true,
-    exclude: ['/server-sitemap.xml'],
+    exclude: ['/server-sitemap.xml', '/admin', '/admin/login'],
     robotsTxtOptions: {
         additionalSitemaps: [
             'https://www.maksv.lv/server-sitemap.xml',
