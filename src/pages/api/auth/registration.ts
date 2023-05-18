9411f4204ff7a7e653f477eae6da4b8b934fd5ce
artemijsS <artjom1408@gmail.com>
2023-05-18 20:11:37 +0300

    registration off

--- src/pages/api/auth/registration.ts
+++ src/pages/api/auth/registration.ts
@@ -4,33 +4,34 @@ import User from '@/models/User';
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ message: 'Method not allowed' });
-  }
-
-  const { username, password } = req.body;
-
-  await dbConnect();
-
-  try {
-    const existingUser = await User.findOne({ username });
-
-    if (existingUser) {
-      return res.status(409).json({ message: 'User already exists' });
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const newUser = new User({
-      username,
-      password: hashedPassword,
-    });
-
-    await newUser.save();
-
-    return res.status(201).json({ message: 'User created successfully' });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({ message: 'Internal Server Error' });
-  }
+    return res.status(500).json({ message: 'Registration is off' });
+// if (req.method !== 'POST') {
+  //   return res.status(405).json({ message: 'Method not allowed' });
+  // }
+  //
+  // const { username, password } = req.body;
+  //
+  // await dbConnect();
+  //
+  // try {
+  //   const existingUser = await User.findOne({ username });
+  //
+  //   if (existingUser) {
+  //     return res.status(409).json({ message: 'User already exists' });
+  //   }
+  //
+  //   const hashedPassword = await bcrypt.hash(password, 10);
+  //
+  //   const newUser = new User({
+  //     username,
+  //     password: hashedPassword,
+  //   });
+  //
+  //   await newUser.save();
+  //
+  //   return res.status(201).json({ message: 'User created successfully' });
+  // } catch (error) {
+  //   console.log(error);
+  //   return res.status(500).json({ message: 'Internal Server Error' });
+  // }
 }
